5b0bb702ceb24007255684c20298e7258919517a
Matteo <matteosabbatini23@gmail.com>
2024-07-03 01:50:43 +0200

    first pages

---
+++ src/app/curriculum/page.jsx
@@ -0,0 +1,8 @@
+export default function Curriculum() {
+    return (
+        <main>
+<h1> This is my pretty Curriculum</h1>
+
+        </main>
+    )
+}
---
+++ src/app/curriculum/programming/page.jsx
@@ -0,0 +1,8 @@
+export default function programming
+() {
+    return (
+        <main>
+            <h1>This section will hold my programming experiences   </h1>
+        </main>
+    )
+}
