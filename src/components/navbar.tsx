c217f0d33ec68df139a5efe798a0e5e73a97a5ea
Matteo <matteosabbatini23@gmail.com>
2024-08-13 02:36:40 +0200

    main navigation p1

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,62 @@
+import { Button } from '@mui/material'
 import Link from 'next/link'
+
 export default function Navbar() {
     return (
-        <nav>
-            <h1>This is the beggining of the side navigation</h1>
-            <ul>
-                <li>
-            <Link href="/">Homepage</Link>
-                </li>
+        <nav className='main_nav'>
+            <div className="nav_open">
+                <ul>
+                    <li>
+                <Link href="/">Homepage</Link>
+                    </li>
 
-                <li>
-            <Link href="/curriculum">Curriculum</Link>
-                </li>
+                    <li>
+                <Link href="/curriculum">Curriculum</Link>
+                    </li>
 
-                <li>
-            <Link href="/projects">Projects</Link>
-                </li>
-                <li>
-                <Link href="/about_me">Su di me</Link>
-                </li>
-                <li><Link href="/contacts">Contatti</Link></li>
-            </ul>
+                    <li>
+                <Link href="/projects">Projects</Link>
+                    </li>
+                    <li>
+                    <Link href="/about_me">Su di me</Link>
+                    </li>
+                    <li><Link href="/contacts">Contatti</Link></li>
+                </ul>
+            </div>  
+            <div className="nav_close">
+                <ul>
+                    <li  id='curriculum'>
+                        <Link href="/curriculum">
+                            <Button>
+                                <span>Curriculum</span> 
+                            </Button>
+                        </Link>
+                    </li>
+
+                    <li id='projects'>
+                        <Link href="/projects">  
+                            <Button>
+                                <span>Progetti</span> 
+                            </Button></Link>
+                    </li>
+
+                    <li id='about'>
+                        <Link href="/about_me">  
+                            <Button>
+                                <span>Su di me</span> 
+                            </Button></Link>
+                    </li>
+
+                    <li id='contacts'>
+                        <Link href="/contacts">
+                            <Button>
+                                <span>Contatti</span> 
+                            </Button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>  
         </nav>
+        
     )
 }
