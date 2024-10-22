65be4a9556a9d34021b8cb790c7590d8be23d4e8
MatteoSabbatini <matteosabbatini23@gmail.com>
2024-10-22 02:15:45 +0200

    HP navigation overlay and style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { Button } from '@mui/material'
-import Link from 'next/link'
-import Image from 'next/image'
 import { useState } from 'react'
+import { Button } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
 
+import CoolTitle from './coolTitle';
 import "./styles/navbar.scss"
 
 export default function Navbar() {
@@ -12,35 +13,40 @@ export default function Navbar() {
         <section className={`${toggleMenu ? "main_nav main_nav_open" : "main_nav main_nav_close"} ` }> 
             <nav className={`${toggleMenu ? "nav_open" : "nav_close"} ` }>
                 <ul>
-                    <li  id='curriculum'>
+                    <li className={`${toggleMenu ? "nav_open" : "nav_closed"} ` }  id='curriculum'>
                         <Link href="/curriculum">
                             <Button>
                                 <span>Curriculum</span> 
                             </Button>
+                           <h3 className={"title"}>Curriculum</h3>
                         </Link>
+
                     </li>
 
-                    <li id='projects'>
+                    <li className={`${toggleMenu ? "nav_open" : "nav_closed"} ` }  id='projects'>
                         <Link href="/projects">  
                             <Button>
                                 <span>Progetti</span> 
                             </Button>
+                            <h3 className={"title"}>Progetti</h3>
                         </Link>
                     </li>
 
-                    <li id='about'>
+                    <li className={`${toggleMenu ? "nav_open" : "nav_closed"} ` } id='about'>
                         <Link href="/about">  
                             <Button>
                                 <span>Su di me</span> 
                             </Button>
+                            <h3 className={"title"}>Su di me</h3>
                         </Link>
                     </li>
 
-                    <li id='contacts'>
+                    <li className={`${toggleMenu ? "nav_open" : "nav_closed"} ` } id='contacts'>
                         <Link href="/contacts">
                             <Button>
                                 <span>Contatti</span> 
                             </Button>
+                            <h3 className={"title"}>Contatti</h3>
                         </Link>
                     </li>
                 </ul>
