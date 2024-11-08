a53716b7ec15669451a3fce3545cda2168c5daf8
MatteoSabbatini <matteosabbatini23@gmail.com>
2024-11-08 01:48:02 +0100

    Final touches to About Page

--- src/components/aboutMap.tsx
+++ src/components/aboutMap.tsx
@@ -10,7 +10,8 @@ export default function AboutMap() {
             <div className="map_container">
                 <CoolTitle
                 title="Nazioni visitate"
-                color="blue_2"/>
+                colorClass="blue_2"/>
+                <p className ="map_intro" >Queste mappe di seguito (3) evidenziano i posti in cui sono stato, sia in visita che per vivere. <br/> Sulla mappa Globale ci sono indicati anche i giorni di permanenza</p>
             
                 <div className="about_map">
                     <Script src="https://static-bundles.visme.co/visme-embed.js" />
