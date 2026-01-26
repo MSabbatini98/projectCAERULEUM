49cfe959fcc04d99321551bcbbfecaa76bf64b46
Matteo <matteosabbatini23@gmail.com>
2026-01-26 16:02:38 +0100

    fix hydration error and installed more stable node version (v22 -> v20). The error was Wappalyzer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" >
       <body >
         <Header />
         <Navbar />
