ed3f5994be023e9028968ea25d41feb0dbc2658b
Matteo <matteosabbatini23@gmail.com>
2026-01-26 22:48:37 +0100

    calculator styles

--- src/app/projects/basic-calculator/page.tsx
+++ src/app/projects/basic-calculator/page.tsx
@@ -1,7 +1,5 @@
-
 import CoolTitle from "@/components/utils/coolTitle"
-import calculatorStyles from './basic-calculator.module.scss'
-
+import "./basic-calculator.css"
 export default function Calculator() {
   return (
     <main className="main_playground"  >
@@ -10,7 +8,7 @@ export default function Calculator() {
         <CoolTitle
             title="Calcolatrice classica"
             colorClass="blue_3"/>
-        <p>This is a basic calculator built with HTML, CSS, and JavaScript. It performs simple arithmetic operations such as addition, subtraction, multiplication, and division.</p>
+        <p>Lorem This is a basic calculator built with React. It performs simple arithmetic operations such as addition, subtraction, multiplication, and division.</p>
 
       <BasicCalculator />
         </div>
@@ -22,7 +20,35 @@ function BasicCalculator() {
 
 
   return (
-    <div className={calculatorStyles.basic_calculator}> 
+    <div className="b_calculator_container">
+      <div className="calculator_grid"> 
+        <div className="output">
+          <div className="prev-oper"></div>
+          <div className="curr-oper"></div>
+
+        </div>
+      <button className="span-two calc_operator">AC</button>
+      <button className="calc_operator">DEL</button>
+      <button className="calc_operator">+</button>
+      <button className="calc_number">1</button>
+      <button className="calc_number">2</button>
+      <button className="calc_number">3</button>
+      <button className="calc_operator">-</button>
+      <button className="calc_number">4</button>
+      <button className="calc_number">5</button>
+      <button className="calc_number">6</button>
+      <button className="calc_operator">*</button>
+      <button className="calc_number">7</button>
+      <button className="calc_number">8</button>
+      <button className="calc_number">9</button>
+      <button className="calc_operator">÷</button>
+      <button className="calc_operator">.</button>
+      <button className="calc_number">0</button>
+      <button className="span-two calc_operator">=</button>
+
+
+      </div>
     </div>
+
   )
 }
