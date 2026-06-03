4ba0530b9ea4110e6960e09d04547de6075cd2d1
Matteo <matteosabbatini23@gmail.com>
2026-06-03 23:15:49 +0200

    Lorem update Quosque

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,8 @@ export default function NotFound() {
     return (
         <div className="404">
             <h2>This page does not exist yet - or ever</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quae quasi doloremque repellendus vero. Quo deleniti, facilis itaque officiis eaque sapiente sint quos aperiam iure, doloribus exercitationem similique, quaerat aut.</p>
+            <p>Quosque tandem abutere, catilina, patentia nostra ? Quamdium nos etiam furor iste tuus eludet ? Quem ad finem sese effrenata jactabit audacia ? nihilne te nocturrnum praesidium palatii, nihil urbis vigiliae, nihil timor populi nihil consensus bonorum omnium nihil hic minitisssimus habendi senatus locus, nihil horum ora vultusque moverunt ?</p>
+            <p>Quosque tandem abutere, catilina, patentia nostra ? Quamdium nos etiam furor iste tuus eludet ? Quem ad finem sese effrenata jactabit audacia ? nihilne te nocturrnum praesidium palatii, nihil urbis vigiliae, nihil timor populi nihil consensus bonorum omnium nihil hic minitisssimus habendi senatus locus, nihil horum ora vultusque moverunt </p>
         </div>
     );
 }
